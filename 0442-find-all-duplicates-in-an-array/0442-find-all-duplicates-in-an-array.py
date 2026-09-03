eeccf43a261f6d2d651b265674d2aa5968b3c2b7
M.Tejas Yadav <tejas.yadav3453@gmail.com>
2026-09-03 15:43:52 +0530

    Time: 38 ms (36.09%), Space: 35.7 MB (5.77%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -1,10 +1,14 @@
-from collections import Counter
 class Solution:
     def findDuplicates(self, nums: List[int]) -> List[int]:
-        freq = dict(Counter(nums))
+        freq={}
+        for i in nums:
+            if i in freq:
+                freq[i]+=1
+            else:
+                freq[i]=1
         res = []
-        # for num in freq.items():
-        #     if freq[num].keys() >=2:
-        #         res.append(freq[num].values())
-        # return res  
-        return [num for num, count in freq.items() if count>=2]  
+        for num,count in freq.items():
+            if count == 2:
+                res.append(num)
+        return res  
+     
